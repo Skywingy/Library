1f849bc7e7754b8bd2b2e93456647d3f37b3f442
Skywingy <nyamka52@yahoo.com>
2022-07-15 17:52:36 +0800

    at this point i have completely started what i was doing, and this is the new start line, i guess

--- script.js
+++ script.js
@@ -1,21 +1,59 @@
-let myLibrary = [6, 21, 53, 46, 53];
+//button event listeners for create new book, add new book to page, close popup
 
-function Book() {
-  // the constructor...
+
+
+
+// book constructor
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
-function addBookToLibrary() {
-  // do stuff here
+
+//creates book from Book Constructor, adds to library
+
+let myLibrary = [];
+
+
+
+function addBookToLibrary(title, author, pages, read) {
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+
 }
 
-for (let i = 0; i < myLibrary.length; i++) {
-    console.table(myLibrary[i]);
+
+//Creates book visual in browser
+
+function render(){
+    const books = document.querySelector(".books");
+
+    myLibrary.forEach(myLibrary => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        books.appendChild(card);
+        for (let key in myLibrary) {
+            console.log(`${key}: ${myLibrary[key]}`);
+            const para = document.createElement("p");
+            para.textContent = (`${key}: ${myLibrary[key]}`);
+            card.appendChild(para);
+        }
+    })
 }
 
-function addBook() {
-    document.getElementById('form1').style.display = 'block';
-}
+addBookToLibrary("The Hobby", "J.R.R Tolkien", "295 page", "Not read yet");
 
-function add() {
-    document.getElementById('form2').style.display = 'block';
-}
+console.log("End of array contents", myLibrary);
+
+
+
+render();
+
+
+//1. we have myLibrary array to store books to be added
+//2. we have constructor object function Book() to construct every book objects
+//3. we have function addBookToLibrary() to push new constructod book objects to be pushed to array
+//4. we have function render() to display all these created objects to browser >> in order to display first we should create element where we will going to display, second we should add class order to distinguish from other elements and last we should append created elements to existing element which is kind of linking part

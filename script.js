02ee3c96361d3ed82b0f689835689593a42b9a28
Skywingy <nyamka52@yahoo.com>
2022-07-18 12:20:47 +0800

    Okay... so far i have done prety much everything, but there has left css part and 1 more functional part which is toggle of READ...

--- script.js
+++ script.js
@@ -5,11 +5,11 @@
 
 // book constructor
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+function Book(Title, Author, Pages, Read) {
+    this.Title = Title;
+    this.Author = Author;
+    this.Pages = Pages;
+    this.Read = Read;
 }
 
 
@@ -19,9 +19,10 @@ let myLibrary = [];
 
 
 
-function addBookToLibrary(title, author, pages, read) {
-    let book = new Book(title, author, pages, read);
+function addBookToLibrary(Title, Author, Pages, Read) {
+    let book = new Book(Title, Author, Pages, Read);
     myLibrary.push(book);
+    render();
 
 }
 
@@ -30,30 +31,86 @@ function addBookToLibrary(title, author, pages, read) {
 
 function render(){
     const books = document.querySelector(".books");
+    
 
-    myLibrary.forEach(myLibrary => {
+    const removeDivs = document.querySelectorAll(".card");
+    console.log("Here", removeDivs);
+    for (let i = 0; i < removeDivs.length; i++) {
+        removeDivs[i].remove();
+    }
+    let index = 0;
+    myLibrary.forEach(myLibrarys => {
         const card = document.createElement("div");
         card.classList.add("card");
+        card.setAttribute("id", "cards");
         books.appendChild(card);
-        for (let key in myLibrary) {
-            console.log(`${key}: ${myLibrary[key]}`);
-            const para = document.createElement("p");
-            para.textContent = (`${key}: ${myLibrary[key]}`);
-            card.appendChild(para);
+
+        const removeB = document.createElement("BUTTON");
+        removeB.classList.add("removeB");
+        const textB = document.createTextNode("Remove");
+        removeB.dataset.linkedArray = index;
+        index++;
+        removeB.appendChild(textB);
+        card.appendChild(removeB);
+        removeB.addEventListener("click", removeBu);
+
+        function removeBu() {
+            let retBookFromLib = removeB.dataset.linkedArray;
+            myLibrary.splice(parseInt(retBookFromLib), 1);
+            card.remove();
+            render();
         }
+
+        for (let key in myLibrarys) {
+            console.log(`${key}: ${myLibrarys[key]}`);
+            const para = document.createElement("p");
+            para.textContent = (`${key}: ${myLibrarys[key]}`);
+            card.appendChild(para);
+        
+        }
+        
     })
 }
 
-addBookToLibrary("The Hobby", "J.R.R Tolkien", "295 page", "Not read yet");
 
-console.log("End of array contents", myLibrary);
-
-
-
-render();
 
 
 //1. we have myLibrary array to store books to be added
 //2. we have constructor object function Book() to construct every book objects
 //3. we have function addBookToLibrary() to push new constructod book objects to be pushed to array
 //4. we have function render() to display all these created objects to browser >> in order to display first we should create element where we will going to display, second we should add class order to distinguish from other elements and last we should append created elements to existing element which is kind of linking part
+
+const addBookButton = document.querySelector(".addBook");
+addBookButton.addEventListener("click", displayTheForm);
+
+
+//displays the form
+function displayTheForm() {
+    document.getElementById("addBookForm").style.display = "";
+};
+
+//intake form data into myLibrary array
+const submitButton = document.querySelector(".submitB");
+submitButton.addEventListener("click", intakeFormData);
+
+function intakeFormData() {
+    let Title = document.getElementById("Title").value;
+    let Author = document.getElementById("Author").value;
+    let Pages = document.getElementById("Pages").value;
+    let Read = document.getElementById("Read").value;
+
+    if ((Title == "") || (Author == "") || (Pages == "") || (Read == "")) {
+        return;
+    }
+
+addBookToLibrary(Title, Author, Pages, Read);
+document.getElementById("add-book").reset();
+
+};
+
+const clearButton = document.querySelector(".resetB");
+clearButton.addEventListener("click", clearForm);
+
+function clearForm() {
+    document.getElementById("add-book").reset();
+}
